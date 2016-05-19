300cdb1428e9f05f7ab972354ba41c397261bd0a
Alfredo Cádiz <acadiz@gmail.com>
2016-05-19 16:42:38 -0400

    updated NodeJS demo

--- nodejs-express/app.js
+++ nodejs-express/app.js
@@ -1,92 +1,74 @@
-const express = require('express'), 
-  app = express(),
-  server = require('http').createServer(app);
+const express = require('express'), app = express(), server = require('http').createServer(app);
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const https = require('https');
-const session = require('express-session');
-var REDIRECT_URL; 
+const uid = require('uid-safe').sync;
+var REDIRECT_URL;
+var CONTENT_PROVIDER_URL='https://www.u-cursos.cl/upasaporte/?';
+var APP_NAME='demo';
+/*
+For demo purposes, you should use more sofisticated means to
+store cookes.
+*/
+var SESSION = {};
 
 //Parser for URL-Encoded body
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-//In this demo we show how can be sessions started
-//Here we configure express-session
-app.use(session({
-  secret: 'SAAN(klnaslkdnasl^&%jahsbd',
-  cookie: {},
-  saveUninitialized: false,
-  resave: false
-}));
-
-//Be define the route to receive the EXTERNAL authenticator
+//We define the route to receive the EXTERNAL authenticator
 //called by U-Pasaporte
 app.post('/external', function(req,res){
-	//We first check the signature
-	verify_signature(req.body, 
-		//Success callback, here you can perform custom
-		//procedures and checks for your service.
-		function(){
-			//Create session
-			//In this demo we show how can be sessions started
-			app.use(session({
-  				secret: 'SAAN(klnaslkdnasl^&%jahsbd',
-  				cookie: {},
-  				saveUninitialized: false,
-  				resave: false
-			}));
-	
+  var url = CONTENT_PROVIDER_URL+'servicio='+APP_NAME+'&ticket='+req.body['ticket'];
+  https.get(url, function(r){
+    var data = "";
+    r.on('data', function(d) {
+      data+=d;
+    });
+    r.on('end', function() {
+      if(r.statusCode != 200){
+        res.send("Surgió un error finalizando la autenticación. Si el problema persiste, contáctese a soporte.");
+        return;
+      }
+      //From here, we know the call was successful and we can parse the data
+      data = JSON.parse(data);
+
+      //Save some data into the session
+      var sess_id = uid(24);
+      SESSION[sess_id] = data;
+
 			//We create the Redirect URL
-			var redirect = REDIRECT_URL;
-			redirect += 'sessid='+req.session.id;
-			redirect += '&alias='+req.body.alias;
+			var redirect = REDIRECT_URL+'sessid='+sess_id;
+      //We return the REDIRECT_URL to U-Pasaporte
 			res.send(redirect);
-		}, 
-		//Failure callback, define the error message
-		function(){
-			res.send('error');
-		});
+    });
+    r.on('error', function(){
+      res.send("Surgió un error finalizando la autenticación. Si el problema persiste, contáctese a soporte.");
+    });
+  });
 });
 
-//Here's is where the successful authentications are redirected, from here you can 
-//re-take the app flow. Remember to set the session in the client.
+/*
+Here's is where the successful authentications are redirected, from here you can
+re-take the app flow.
+We use the simple SESSION array to store and retrieve values, you
+should use something more sofisticated.
+*/
 app.get('/authenticated', function(req, res){
-	res.send(JSON.stringify(req.query));
-})
-
-/**
-Verify U-Pasaporte signatures
-@param params the POST parameters sent from U-Pasaporte
-@param success block called on success verifications
-@param faulure block called on failed verifications
-**/
-function verify_signature(params, success, failure){
-	const firma = params.firma;
-	delete params.firma;
-	https.get('https://www.u-cursos.cl/upasaporte/certificado', function(r){
-        	var public_key;
-        	r.on('data', function(d) {
-                        public_key =d;
-                });
-                r.on('end', function() {
-			const signature = Object.keys(params).sort().map(function(key){return decode(params[key])}).reduce(function(p,c){return p+c});
-			const verify = crypto.createVerify('RSA-SHA1');
-			console.log(signature);
-			verify.update(signature);
-			if(verify.verify(public_key, firma, 'base64')) success();
-			else failure();
-                });
-        });	
-}
-
-function decode(str) {
-     return unescape(str.replace(/\+/g, " "));
-}
+  var sess = SESSION[req.query.sessid];
+  res.write("<html><head><meta charset='utf-8'></head><body>");
+	res.write("<h1>Hola! "+sess.alias+"</h1><br/>");
+  res.write("<h2>Session ID: "+req.query.sessid+"</h2><br/>");
+  res.write("<h3>Datos recibidos: </h3><br/>");
+  res.write("<pre>");
+  res.write(JSON.stringify(sess, null, 4));
+  res.write("</pre>");
+  res.write("</body></html>");
+  res.end();
+});
 
 server.listen(3000, function(){
-	const port = server.address().port === 80 ? "" : ":"+server.address().port;	
+	const port = server.address().port === 80 ? "" : ":"+server.address().port;
 	REDIRECT_URL = "http://"+require('os').hostname()+port+"/authenticated?";
 	console.log('running at port 3000');
 	console.log("Redirect URL: "+REDIRECT_URL);
